9ca8f7375dccd4a13ca6f8d93964ee4dd7773f9c
core.ui <roman.thimian@me.com>
2024-02-05 22:33:30 +0100

    Key mapping move line and blocks

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,11 +5,15 @@ vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
 -- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>h", ':nohlsearch<CR>')
+vim.keymap.set("n", "<CS-j>", ":m .+1<CR>")
+vim.keymap.set("n", "<CS-k>", ":m .-2<CR>")
+vim.keymap.set("v", "<CS-j>", ":m '>+1<CR>gv")
+vim.keymap.set("v", "<CS-k>", ":m '<-2<CR>gv")
 
 vim.wo.number = true
 vim.wo.relativenumber = true
