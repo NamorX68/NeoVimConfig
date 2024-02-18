f41c879dbb1bb29d509c633077e1538af17f0633
core.ui <roman.thimian@me.com>
2024-02-18 02:22:03 +0100

    New Keybindings

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,20 +1,20 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.black.with({
-          extra_args = { "--line-length=130" }
-        }),
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-      },
-    })
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.black.with({
+                    extra_args = { "--line-length=130" },
+                }),
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.diagnostics.ruff,
+                null_ls.builtins.formatting.rustfmt,
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+            },
+        })
 
-    vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
-  end,
+        vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format, {})
+    end,
 }
