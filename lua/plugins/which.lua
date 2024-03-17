51803e105488a656ed0de8807496c56dc66884df
core.ui <roman.thimian@me.com>
2024-03-17 13:08:57 +0100

    Remove LazyGit from whichkey

--- lua/plugins/which.lua
+++ lua/plugins/which.lua
@@ -67,7 +67,6 @@ return {
 
             ["<leader>g"] = {
                 name = "Git",
-                g = { "<cmd>lua require 'lvim.core.terminal'.lazygit_toggle()<cr>", "Lazygit" },
                 j = { "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", "Next Hunk" },
                 k = { "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", "Prev Hunk" },
                 l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
